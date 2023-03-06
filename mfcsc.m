3504501253c15e65f1198a6af12418ec0d3c814e
Oren Civier <orenciv@gmail.com>
2023-03-07 02:23:42 +1100

    Update mfcsc.m

--- mfcsc.m
+++ mfcsc.m
@@ -1,5 +1,28 @@
 function mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep_neg_fc,is_symmetrical,is_figures,BCT_DIR)
-%%% 
+%%%
+%%%     mfcsc -- Mismatch between Functional Connectivity and Structural Connectivity  
+%%%     =============================================================================
+%%%
+%%%     USAGE
+%%%
+%%%         From Matlab IDE:
+%%%             Change to the folder with the code, and then enter the following command -
+%%%             mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,not_in_mask_value,is_contra,is_keep_neg_fc,is_symmetrical,is_figures,bct_dir)         
+%%%
+%%%         From the command-line:
+%%%             Change to the folder with the code, and then enter the following command -
+%%%             matlab -batch "mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,not_in_mask_value,is_contra,is_keep_neg_fc,is_symmetrical,is_figures,bct_dor)"
+%%%
+%%%         The 4 arguments in caps are mandatory. 
+%%%
+%%%         IMPORTANT: before running mfcsc, it is recommended to open FC_SC_LIST 
+%%%         and visually check that the two columns have matching
+%%%         participant connectomes.
+%%%
+%%%     PREREQUISITES:
+%%%
+%%%         Matlab's Curve Fitting toolbox
+%%%
 %%%     DESCRIPTION
 %%%
 %%%         mfcsc receive pairs of functional and structural connectivity
@@ -13,6 +36,8 @@ function mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep
 %%%         indicates which mFCSC metrics should be included in further analysis. 
 %%%         The cells that should not be consulted further are set to -99.
 %%%
+%%%     INPUT
+%%%
 %%%         The input connectivity matrices should be comma seperated values that describe symmetrical 
 %%%         connectomes (i.e., connections do not have directionality). 
 %%%         Only the upper right triangle of the connectivity matrices is
@@ -23,29 +48,51 @@ function mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep
 %%%         Note that the order of regions for each hemisphere may be different, i.e., 
 %%%         regions number i and N/2+i do not have to be homologous.
 %%%
-%%%     USAGE
+%%%     OUTPUT
 %%%
-%%%         1)  Make sure that your Matlab instllation includes the Curve Fitting toolbox
+%%%         IMPORTANT: before consulting the output, ensure that the file1 and file2 parts 
+%%%         of the filenames below represent matching connectivity
+%%%         matrices!
+%%%         
+%%%         The output of mfcsc consists of one connectome file for each participant:
 %%%
-%%%         2)  Organise input files and generate a FC-SC list (see ARGUMENTS below)
+%%%                 mFCSC-file1-file2-masked.csv - connectome of mFCSC values for the
+%%%                                                participant whose FC and SC
+%%%                                                connectomes are stored in file1 and file2
+%%%                                                respectively (excluding
+%%%                                                the files extensions)
 %%%
-%%%         3)  Verify that the each pair in the FC-SC list is made up of two matching connectomes
+%%%         The main output of mfcsc consists of the file:
 %%%
-%%%         4)  From Matlab IDE:
-%%%             Change to the folder with the code, and then enter the following command -
-%%%             mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep_neg_fc,is_symmetrical,is_figures,BCT_DIR)         
+%%%                 mask-final.csv - final mask indicating the connections to which mFCSC is calculated
 %%%
-%%%             From the command-line:
-%%%             Change to the folder with the code, and then enter the following command -
-%%%             matlab -batch "mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep_neg_fc,is_symmetrical,is_figures,BCT_DIR)"
+%%%         There are also several misc files in the group_connectomes subdir:
 %%%
-%%%     ARGUMENTS (mandatory)
+%%%             transformed_SC_avg - the average transfered SC connectome
+%%%             FC_avg - the average FC connectome
+%%%             mask-direct_SC_is_shortest_path.csv - mask of connections in which the path length of the direct connection (1/transformed_SC) is shorter than any other indirect path between the two regions
+%%%             SC_avg - the average SC connectome
+%%%
+%%%     MANDATORY ARGUMENTS
 %%%
 %%%         FC_SC_LIST
 %%%
-%%%         Full path to a tab-separated file with two columns.
+%%%         Path to a tab-separated file with two columns.
 %%%         The first column lists the files with the connectivity matrices of the FC connectomes
 %%%         The second column lists the connectivity matrix files for the matching SC connectomes
+%%%
+%%%         An easy method to generate the FC_SC_LIST on Linux or MacOS is to:
+%%%         1) include a matching participant ID in both FC and SC connectomes 
+%%%            (plus optional fixed prefixes and suffixes for each modality).
+%%%         2) put all FC connectomes in one folder and all SC connectomes in another,
+%%%            ensuring that there are no other files in these folders.
+%%%         3) change the working directory to the folder with FC connectomes, and run:
+%%%             ls | sort -n > /tmp/fc_list
+%%%         4) Change the working directory to the folder with the SC connectomes, and run:
+%%%             ls | sort -n > /tmp/sc_list
+%%%         5) Run:
+%%%             paste /tmp/fc_list /tmp/sc_list > path_to_filename
+%%%         6) Provide path_to_filename as the FC_SC_LIST argument
 %%%
 %%%         FC_INPUT_DIR
 %%%
@@ -60,7 +107,7 @@ function mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep
 %%%         The output directory where the mask and mFCSC files are to be
 %%%         saved
 %%%
-%%%     FLAGS (optional)
+%%%     OPTIONAL ARGUMENTS  
 %%%
 %%%         is_contra
 %%%         
@@ -91,28 +138,13 @@ function mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep
 %%%         set is_figures to true to print verbose figures with information for QC and debg.
 %%%         Not tested. Use at your own risk!
 %%%
-%%%         BCT_DIR
+%%%         bct_dir
 %%%
 %%%         If specified, Matlab will look for the Brain Connectome Toolbox (BCT)
 %%%         in this directory instead of the BCT version supplied with
 %%%         MFCSC (2017/01/15). BCT is required in order to calculate the mask.
-%%%
-%%%     OUTPUT
-%%%
-%%%         The main output of mfcsc consists of the file:
-%%%                 mask-final.csv - final mask indicating the connections to which mFCSC is calculated
-%%%         As well as one connectome file for each participant:
-%%%                 mFCSC-file1-file2-masked.csv - connectome of mFCSC values for the
-%%%                                                participant whose FC and SC
-%%%                                                connectomes are stored in file1 and file2
-%%%                                                respectively (excluding
-%%%                                                the files extensions)
-%%%
-%%%         There are also several misc files in the misc subdir:
-%%%             transformed_SC_avg - the average transfered SC connectome
-%%%             FC_avg - the average FC connectome
-%%%             mask-direct_SC_is_shortest_path.csv - mask of connections in which the path length of the direct connection (1/transformed_SC) is shorter than any other indirect path between the two regions
-%%%             SC_avg - the average SC connectome
+%%%         This argument is not available in the Neurodesk version, where only the supplied
+%%%         BCT can be used.
 %%%
 %%%     NOTE
 %%%         
@@ -130,12 +162,11 @@ function mfcsc(FC_SC_LIST,FC_INPUT_DIR,SC_INPUT_DIR,OUTPUT_DIR,is_contra,is_keep
 %%%
 %%%	    When using MFCSC, the following articles should be cited:
 %%%
-%%%		MFCSC: Novel method to calculate mismatch between functional and structural brain connectomes, and its application for 
-%%%		detecting hemispheric functional specialisations
-%%%		Civier O, Sourty M, Calamante F (in press) Scientific Reports https://doi.org/10.1038/s41598-022-17213-z
+%%%		Civier O, Sourty M, Calamante F (2023) MFCSC: Novel method to calculate mismatch between functional and structural brain connectomes, and its application for 
+%%%		detecting hemispheric functional specialisations. Scientific Reports https://doi.org/10.1038/s41598-022-17213-z
 %%%
-%%%         	Complex network measures of brain connectivity: Uses and interpretations.
-%%%		Rubinov M, Sporns O (2010) NeuroImage 52:1059-69.
+%%%     Rubinov M, Sporns O (2010) Complex network measures of brain connectivity: Uses and interpretations.
+%%%		NeuroImage 52:1059-69.
 %%%
 
 % OC - Mac/Linux example - remove from final version
