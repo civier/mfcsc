b1c0cb41c0fe227c8a4cd15e8bec0d015f647771
Oren Civier <orenciv@gmail.com>
2023-03-06 18:20:37 +1100

    Delete cmtk_to_mrtrix3_order.m

--- cmtk_to_mrtrix3_order.m
+++
@@ -1,8 +0,0 @@
-function [sc,labels] = cmtk_to_mrtrix3_order(in_sc,in_labels)
-
-FULL_IDX = get_index;
-for i=1:length(FULL_IDX)
-      REV_IDX(FULL_IDX(i)) = i;
-end 
-
-[sc, labels] = reorder(in_sc,in_labels, REV_IDX);
