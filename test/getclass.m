fbcbb525a0c7eb350352ee7bed234062fc1f843f
Oren Civier <orenciv@gmail.com>
2023-03-06 18:21:40 +1100

    Add files via upload

---
+++ test/getclass.m
@@ -0,0 +1,14 @@
+function index = getclass(str,strs)
+
+    ind=find(ismember(strs,'KU'))
+
+if isstring(arg)
+    for index=1:length(strs)
+        if strcmp(arg,strs{index})
+            return;
+        end
+        error('Internal error. Contact developers');
+    end
+else
+    
+    
