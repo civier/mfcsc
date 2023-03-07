19a23f9a842fde1281e72fc9f12c5d6ffd7ca788
Oren Civier <orenciv@gmail.com>
2023-03-07 19:44:01 +1100

    Update testlat.m

--- test/testlat.m
+++ test/testlat.m
@@ -87,37 +87,24 @@ function testlat(input_folder,output_folder)
 %
 %             The directory where the output files will be written to.
 %
+%      DEVELOPER
 %
-%      For HCP data in the respository, there are 512 connections that are
-%      1 in the left hemisphere, and 497 connections that are 1 in the
-%      right hemisphere. However, their overlap only gives 475 connections
-%      which are 1 in both hemispheres.
+%             Oren Civier (orenciv@gmail.com)
+%             https://www.swinburne.edu.au/research/our-research/access-our-research/find-a-researcher-or-supervisor/researcher-profile/?id=ocivier
 %
-%      B & E (flips) has the most significant p-values that are not in the
-%      mask. This is because such a big difference usually does not exist,
-%      and if it does, it is usually due to FC in the strong side being affected by
-%      strong indirect structural connections.
-%      D (L < R in both sides) has tons of significant p-values that are
-%      not in the mask. I'm not sure why? Maybe there are low FC in both
-%      because there is no effective connectivity in both, and the relatively stronger
-%      SC is not real; it is actually weaker than the indirect structural connectivity.
+%      CITATIONS
 %
-%      The only difference from the article is in sig_neg_L_st_R (Fig. 3,
-%      panel D). If running the code, it is:
-%           ctx-lh-parsopercularis - Left-Putamen
-%           ctx-lh-parstriangularis - Left-Putamen
-%           ctx-lh-medialorbitofrontal - Left-Amygdala
-%      But in the paper, it is only the first two:
-%           ctx-lh-parsopercularis - Left-Putamen
-%           ctx-lh-parstriangularis - Left-Putamen           
-%      The difference is due a slight difference in the implementation
-%      between the code provided here and that used for the paper. Here the
-%      linear regression inside each individual includes all connections
-%      where direct SC is the shortest structural path. In contrast, in the
-%      paper, the linear regression only includes the connections where
-%      direct SC is the shortest structural path *in both hemispheres*.
+%             When using testlat, authors should cite:
+%             Civier O, Sourty M, Calamante F (2023) MFCSC: Novel method to calculate mismatch between functional and structural brain connectomes, 
+%             and its application for detecting hemispheric functional specialisations. Scientific Reports https://doi.org/10.1038/s41598-022-17213-z
 %
-
+%      ACKNOWLEDGMENTS
+%
+%             National Health and Medical Research Council of Australia (grant numbers APP1091593 andAPP1117724)
+%             Australian Research Council (grant number DP170101815)
+%             National Imaging Facility (NIF), a National Collaborative Research Infrastructure Strategy (NCRIS) capability at Swinburne Neuroimaging, 
+%             Swinburne University of Technology.
+%
 
 %dbstop if error;
 
